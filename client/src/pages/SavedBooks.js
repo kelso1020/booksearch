cb52b621e9a53b716f43e822c9fa2b449626f030
kelso1020 <kelseyjessica1710@gmail.com>
2023-09-12 18:56:05 -0400

    fixed typos

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -3,23 +3,21 @@ import {
   Container,
   Card,
   Button,
-  Row,
-  Col
-} from 'react-bootstrap';
-
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import { REMOVE_BOOK } from '../utils/mutations';
-import { GET_ME } from '../utils/queries';
+  Row
+} from "react-bootstrap";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
 const SavedBooks = () => {
-  const {loading, data} = useQuery(GET_ME);
-  const userData = data?.me || {};
+  const { loading, data } = useQuery(GET_ME);
+  let userData = data?.me || {};
+  console.log(userData);
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  // function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -28,15 +26,13 @@ const SavedBooks = () => {
     }
 
     try {
-      await removeBook({
-        variables: { bookId }
-      });      
+      const { user } = await removeBook({
+        variables: {
+          bookId: bookId,
+        },
+      });
 
-      if (error) {
-        throw new Error('something went wrong!');
-      }
-
-      // upon success, remove book's id from localStorage
+      userData = user;
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
@@ -56,27 +52,36 @@ const SavedBooks = () => {
         </Container>
       </div>
       <Container>
-        <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
+        <h2>
+          {userData.savedBooks?.length
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
+              }:`
+            : "You have no saved books!"}
         </h2>
         <Row>
           {userData.savedBooks.map((book) => {
             return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                      Delete this Book!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
+              <Card key={book.bookId} border="dark">
+                {book.image ? (
+                  <Card.Img
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                    variant="top"
+                  />
+                ) : null}
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <p className="small">Authors: {book.authors}</p>
+                  <Card.Text>{book.description}</Card.Text>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteBook(book.bookId)}
+                  >
+                    Delete this Book!
+                  </Button>
+                </Card.Body>
+              </Card>
             );
           })}
         </Row>
