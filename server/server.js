02cd47d8fb4cd9eb97a7ffb278a652a9da1da50e
kelso1020 <kelseyjessica1710@gmail.com>
2023-09-12 20:15:52 -0400

    fix typo in server.js

--- server/server.js
+++ server/server.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-// import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
-// import typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+
+// add apollo server
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// create a new Apollo server and pass in schema data
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
-// integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app });
+// add apollo middleware
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
 
-server();
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
